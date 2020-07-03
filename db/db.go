1aa69c3927de9d7cabc96bdb7e1a8dd8b5baaed1
takeru0430 <t.nezu@nextreservation.jp>
2020-07-03 10:50:52 +0900

    コミット漏れしたファイルの登録

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import "database/sql"
+import "log"
+import "os"
+import _ "github.com/go-sql-driver/mysql"
+import env "github.com/joho/godotenv"
+
+func connect() *sql.DB {
+    env.Load("../.env")
+
+    // 環境変数の読み込みとセット
+    u := os.Getenv("DB_USER")
+    p := os.Getenv("DB_PASS")
+    h := os.Getenv("DB_HOST")
+    n := os.Getenv("DB_NAME")
+
+    // DBに接続
+    db, err := sql.Open("mysql", u + ":" + p + "@tcp(" + h + ")/" + n)
+    if err != nil {
+        // エラー時の処理
+        log.Fatal("err:DB接続エラー %v", err)
+    }
+
+    return db
+}
+
+// 外部からこの接続を利用してSQLを流せるようにする
+var Db *sql.DB = connect()
+
