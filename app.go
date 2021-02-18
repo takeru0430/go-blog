05f2230c3a12fbbd28cafa3fe209a89af7677217
Takeru Nezu <t.nezu.private@gmail.com>
2021-02-18 20:51:40 +0900

    Update app.go

--- app.go
+++ app.go
@@ -1,18 +1,13 @@
 package main
 
-// import "log"
 import "net/http/cgi"
 import "go-blog/routes"
 import env "github.com/joho/godotenv"
 
 func main() {
-    //http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("public/"))))
     env.Load()
 
-    // ルーティングのセット
-    //if routes.Set() != true {
-    //    log.Printf("エラー：ルーティングに問題があります。")
-    //}
+    // ルーティングの初期化
     routes.Set()
 
     // listen
